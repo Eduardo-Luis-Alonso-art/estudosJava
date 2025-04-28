d4bcc1803ba28e1e716f87faa6535e3512473267
Eduardo Luis Alonso <eduardoluisalonsi@gmail.com>
2025-04-28 17:32:04 -0300

    Add files via upload

---
+++ src/MeuPrimeiroPrograma.java
@@ -0,0 +1,8 @@
+public class MeuPrimeiroPrograma {
+
+    //CRIAÇÃO DA MAIN(EXECUTA O CÓDIGO)
+    public static void main(String[] args) {
+        //SOUT(PODE MOSTRAR MENSAGENS PARA O USUARIO)
+        System.out.println("Olá Mundo");
+    }
+}
---
+++ src/TiposDeVariaveis.java
@@ -0,0 +1,13 @@
+public class TiposDeVariaveis {
+
+    public static void main(String[] args) {
+        //int (DEFINE UMA VARIAVEL DO TIPO INTEIRO)
+        int idade;
+
+        //DECLARANDO O VALOR DA VARIAVEL
+        idade = 23;
+
+        //MOSTRANDO A IDADE
+        System.out.println(idade);
+    }
+}
